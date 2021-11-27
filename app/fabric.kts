d43c05602094a193491aa3bd4f329a87bd0409b4
zakhep66 <sur.mikhael@gmail.com>
2021-11-27 13:58:04 +0300

    fabric

---
+++ app/fabric.kts
@@ -0,0 +1,43 @@
+
+import java.awt.Checkbox
+
+
+/*
+Абстрактная фабрика — это порождающий паттерн проектирования, который решает проблему создания целых семейств связанных продуктов,
+без указания конкретных классов продуктов.
+ */
+
+
+/*
+В роли двух семейств продуктов выступают кнопки и чекбоксы
+ */
+
+
+interface Button {
+    fun paint()
+}
+
+interface Checkbox {
+    fun paint()
+}
+
+
+interface GUIFactory {
+    fun createButton(): Button?
+    fun createCheckbox(): Checkbox?
+}
+
+
+class Application(factory: GUIFactory) {
+    private val button: Button?
+    private val checkbox: java.awt.Checkbox?
+    fun paint() {
+        button!!.paint()
+        checkbox!!.paint()
+    }
+
+    init {
+        button = factory.createButton()
+        checkbox = factory.createCheckbox()
+    }
+}
